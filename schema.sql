5eddca9e7570f041cabeda8f5c448aa9bab11cc3
Nylbert Victor <nyldrift@hotmail.com>
2023-07-12 17:58:05 -0300

    DB creation and population
    DB created
    DB Populated
    query executed properly

---
+++ schema.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE vet_clinic;
+
+\c vet_clinic;
+
+CREATE TABLE animals (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR,
+  date_of_birth DATE,
+  escape_attempts INTEGER,
+  neutered BOOLEAN,
+  weight_kg DECIMAL
+);
