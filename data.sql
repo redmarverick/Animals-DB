13edeae9db79ebb7b01de0aaa616f64180a82177
Nylbert Victor <nyldrift@hotmail.com>
2023-07-13 17:57:01 -0300

    new tables
    - Add new tables species and owners
    - Remove species from animals table
    - Add keys to bind Animals to species and owners
    - Add data to species and owners
    - Add respective keys to animals species and owner ids
    - Added queries

--- data.sql
+++ data.sql
@@ -10,7 +10,7 @@ VALUES ('Pikachu', '2021-01-07', 1, false, 15.04);
 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
 VALUES ('Devimon', '2017-05-12', 5, true, 11);
-*/
+
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
 VALUES ('Charmander', '2020-02-08', 0, false, -11);
 
@@ -30,4 +30,92 @@ INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
 VALUES ('Blossom', '1998-10-13', 3, true, 17);
 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
-VALUES ('Ditto', '2022-05-14', 4, true, 22);*/
+VALUES ('Ditto', '2022-05-14', 4, true, 22);
+*/
+
+/*
+Insert the following data into the owners table:
+Sam Smith 34 years old.
+Jennifer Orwell 19 years old.
+Bob 45 years old.
+Melody Pond 77 years old.
+Dean Winchester 14 years old.
+Jodie Whittaker 38 years old.
+*/
+INSERT INTO owners (full_name, age)
+VALUES ('Sam Smith', 34);
+
+INSERT INTO owners (full_name, age)
+VALUES ('Jennifer Orwell', 19);
+
+INSERT INTO owners (full_name, age)
+VALUES ('Bob', 45);
+
+INSERT INTO owners (full_name, age)
+VALUES ('Melody Pond', 77);
+
+INSERT INTO owners (full_name, age)
+VALUES ('Dean Winchester', 14);
+
+INSERT INTO owners (full_name, age)
+VALUES ('Jodie Whittaker', 38);
+
+/*
+Insert the following data into the species table:
+Pokemon
+Digimon
+*/
+
+INSERT INTO species (name)
+VALUES ('Pokemon');
+
+INSERT INTO species (name)
+VALUES ('Digimon');
+
+/*
+Modify your inserted animals so it includes the species_id value:
+If the name ends in "mon" it will be Digimon
+All other animals are Pokemon
+*/
+
+UPDATE animals
+SET species_id = (
+  CASE
+    WHEN name LIKE '%mon' THEN (SELECT id FROM species WHERE name = 'Digimon')
+    ELSE (SELECT id FROM species WHERE name = 'Pokemon')
+  END
+);
+
+/*
+Modify your inserted animals to include owner information (owner_id):
+Sam Smith owns Agumon.
+Jennifer Orwell owns Gabumon and Pikachu.
+Bob owns Devimon and Plantmon.
+Melody Pond owns Charmander, Squirtle, and Blossom.
+Dean Winchester owns Angemon and Boarmon.
+*/
+
+-- Update owner_id for Agumon (owned by Sam Smith)
+UPDATE animals
+SET owner_id = (SELECT id FROM owners WHERE full_name = 'Sam Smith')
+WHERE name = 'Agumon';
+
+-- Update owner_id for Gabumon and Pikachu (owned by Jennifer Orwell)
+UPDATE animals
+SET owner_id = (SELECT id FROM owners WHERE full_name = 'Jennifer Orwell')
+WHERE name IN ('Gabumon', 'Pikachu');
+
+-- Update owner_id for Devimon and Plantmon (owned by Bob)
+UPDATE animals
+SET owner_id = (SELECT id FROM owners WHERE full_name = 'Bob')
+WHERE name IN ('Devimon', 'Plantmon');
+
+-- Update owner_id for Charmander, Squirtle, and Blossom (owned by Melody Pond)
+UPDATE animals
+SET owner_id = (SELECT id FROM owners WHERE full_name = 'Melody Pond')
+WHERE name IN ('Charmander', 'Squirtle', 'Blossom');
+
+-- Update owner_id for Angemon and Boarmon (owned by Dean Winchester)
+UPDATE animals
+SET owner_id = (SELECT id FROM owners WHERE full_name = 'Dean Winchester')
+WHERE name IN ('Angemon', 'Boarmon');
--- schema.sql
+++ schema.sql
@@ -9,7 +9,65 @@ CREATE TABLE animals (
   escape_attempts INTEGER,
   neutered BOOLEAN,
   weight_kg DECIMAL
-);*/
+);
 
 ALTER TABLE animals
 ADD COLUMN species VARCHAR;
+*/
+
+
+/* Create a table named owners with the following columns:
+id: integer (set it as autoincremented PRIMARY KEY)
+full_name: string
+age: integer
+vvvvvvvvv*/
+
+CREATE TABLE owners (
+  id SERIAL PRIMARY KEY,
+  full_name VARCHAR,
+  age INTEGER
+);
+
+/*
+Create a table named species with the following columns:
+id: integer (set it as autoincremented PRIMARY KEY)
+name: string
+*/
+
+CREATE TABLE species (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR
+);
+
+/*
+Modify animals table:
+Make sure that id is set as autoincremented PRIMARY KEY
+Remove column species
+Add column species_id which is a foreign key referencing species table
+Add column owner_id which is a foreign key referencing the owners table
+*/
+
+-- Remove the species column from the animals table
+ALTER TABLE animals
+DROP COLUMN species;
+
+-- Modify the id column to be auto-incremented primary key if not already set
+ALTER TABLE animals
+ALTER COLUMN id SET DEFAULT nextval('animals_id_seq');
+
+-- Add the species_id column as a foreign key referencing the species table
+ALTER TABLE animals
+ADD COLUMN species_id INTEGER,
+ADD CONSTRAINT fk_species_id
+    FOREIGN KEY (species_id)
+    REFERENCES species(id);
+
+-- Add the owner_id column as a foreign key referencing the owners table
+ALTER TABLE animals
+ADD COLUMN owner_id INTEGER,
+ADD CONSTRAINT fk_owner_id
+    FOREIGN KEY (owner_id)
+    REFERENCES owners(id);
+
+
+
