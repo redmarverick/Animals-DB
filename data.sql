f6020054863b62282f5d03399c9c2c42981d8c1e
Nylbert Victor <nyldrift@hotmail.com>
2023-07-20 03:17:52 -0300

    move queries
    - move queries to respective files

--- data.sql
+++ data.sql
@@ -156,7 +156,6 @@ Boarmon visited Maisy Smith on Feb 27th, 2020.
 Boarmon visited Maisy Smith on Aug 3rd, 2020.
 Blossom visited Stephanie Mendez on May 24th, 2020.
 Blossom visited William Tatcher on Jan 11th, 2021.
-*/
 
 -- Insert data for vets
 INSERT INTO vets (name, age, date_of_graduation)
@@ -244,3 +243,13 @@ VALUES ((SELECT id FROM animals WHERE name = 'Blossom'), (SELECT id FROM vets WH
 
 INSERT INTO visits (animal_id, vet_id, visit_date)
 VALUES ((SELECT id FROM animals WHERE name = 'Blossom'), (SELECT id FROM vets WHERE name = 'William Tatcher'), '2021-01-11');
+*/
+
+-- Add an email column to your owners table
+ALTER TABLE owners ADD COLUMN email VARCHAR(120);
+
+-- This will add 3.594.280 visits considering you have 10 animals, 4 vets, and it will use around ~87.000 timestamps (~4min approx.)
+INSERT INTO visits (animal_id, vet_id, visit_date) SELECT * FROM (SELECT id FROM animals) animal_ids, (SELECT id FROM vets) vets_ids, generate_series('1970-01-01'::timestamp, '2021-01-01', '4 hours') visit_timestamp ON CONFLICT DO NOTHING;
+
+-- This will add 2.500.000 owners with full_name = 'Owner <X>' and email = 'owner_<X>@email.com' (~2min approx.)
+insert into owners (full_name, email) select 'Owner ' || generate_series(1,2500000), 'owner_' || generate_series(1,2500000) || '@mail.com';
--- queries.sql
+++ queries.sql
@@ -273,17 +273,6 @@ ORDER BY COUNT(*) DESC
 LIMIT 1;
 */
 
--- Add an email column to your owners table
-ALTER TABLE owners ADD COLUMN email VARCHAR(120);
-
--- This will add 3.594.280 visits considering you have 10 animals, 4 vets, and it will use around ~87.000 timestamps (~4min approx.)
-INSERT INTO visits (animal_id, vet_id, visit_date) SELECT * FROM (SELECT id FROM animals) animal_ids, (SELECT id FROM vets) vets_ids, generate_series('1970-01-01'::timestamp, '2021-01-01', '4 hours') visit_timestamp ON CONFLICT DO NOTHING;
-
-
-
--- This will add 2.500.000 owners with full_name = 'Owner <X>' and email = 'owner_<X>@email.com' (~2min approx.)
-insert into owners (full_name, email) select 'Owner ' || generate_series(1,2500000), 'owner_' || generate_series(1,2500000) || '@mail.com';
-
 EXPLAIN ANALYZE SELECT COUNT(*) FROM visits where animal_id = 12;
 
 EXPLAIN ANALYZE SELECT * FROM visits where vet_id = 2;
